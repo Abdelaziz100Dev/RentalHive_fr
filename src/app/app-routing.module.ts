212c7dd64318e993980fbdf00ad108fc4973d2fd
ayoub ouabi <ayoub.ouabi06@gmail.com>
2023-12-05 16:01:16 +0100

    working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {ProductsComponent} from "./products/products.component";
-import {NewProductsComponent} from "./new-products/new-products.component";
 import {TypeComponent} from "./type/type.component";
 import {EquipmentComponent} from "./equipment/equipment.component";
 
 const routes: Routes = [
-  {path : "home", component: HomeComponent},
-  {path : "products", component: ProductsComponent},
-  {path : "new-products", component: NewProductsComponent},
   {path : "equipments", component: EquipmentComponent},
   {path : "types", component: TypeComponent}
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'firstDemo';
   actions: Array<any>=[
-    {"title":"home","route":"/home",icon: "house"},
-    {"title":"products","route":"/products",icon: "arrow-down-up"},
     {"title":"types","route":"/types",icon: "tags"},
-    {"title":"new products","route":"/new-products",icon: "plus-circle"},
     {"title":"equipments","route":"/equipments",icon: "plus-circle"}
   ];
 
